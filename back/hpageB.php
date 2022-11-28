d4ebcd92e6bea73beb6bfe2d56ef181fd79d7d68
CharlesDESC <charles.deschamps@ynov.com>
2022-11-28 14:13:35 +0100

    pouet

--- back/hpageB.php
+++ back/hpageB.php
@@ -5,16 +5,17 @@
 
     $sql = 'SELECT * FROM collection WHERE user_ID = ' . '"' . $_SESSION['ID'] . '"';
 
+
     $query = $db->prepare($sql);
 
     $query->execute();
 
-    $data = $query->fetch();
     $row = $query->rowCount();
+    $data = $query->fetchAll();
 
     $_SESSION['nbcard'] = $row;
 
-    echo $_SESSION['nbcard'];
+    
 
 
 
@@ -23,8 +24,9 @@ if (isset($_GET['name']) && isset($_GET['price']) && isset($_GET['description'])
         $name = htmlspecialchars($_GET['name']);
         $price = htmlspecialchars($_GET['price']);
         $description = htmlspecialchars($_GET['description']);
+        $image = file_get_contents($_GET['picture']);
 
-        $sql = 'INSERT INTO collection (user_ID, name, price, description) VALUE ("' . $_SESSION['ID'] . '","' . $name . '","' . $price . '","' . $description . '")';
+        $sql = 'INSERT INTO collection (user_ID, name, price, description, picture) VALUE ("' . $_SESSION['ID'] . '","' . $name . '","' . $price . '","' . $description . '","' . $image . '")';
 
         $query = $db->prepare($sql);
     
--- front/hpage.php
+++ front/hpage.php
@@ -1,86 +1,85 @@
 <?php
-    include '../back/hpageB.php';
+include '../back/hpageB.php';
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" >
-    <title>Document</title>
-    <link rel="stylesheet" href="../CSS/hpage.css">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="../CSS/hpage.css">
 </head>
+
 <body>
 
-    <nav id="topnav">
-        <a class="nav-link" href="#">Ycollector</a>
-        <a id="logo" class="usr" href="#">
-            <?php 
-                echo $_SESSION['name'];
-            ?>
-        </a>
-        <hr>
-    </nav>
+  <nav id="topnav">
+    <a class="nav-link" href="#">Ycollector</a>
+    <a id="logo" class="usr" href="#">
+      <?php
+      echo $_SESSION['name'];
+      ?>
+    </a>
+    <hr>
+  </nav>
 
-<div id="image">
+  <div id="image">
 
-  <button id="add"> <img src="https://media.discordapp.net/attachments/950364547372425256/1034095033818828800/images.jpeg" > </button>
-  <a id="ok" class="prix">prix</a>
-</div>
-
-<div id="form-item" class="hidden">
-  <form method=”POST” id="form">
-    <div id="name-item">
-      <input type="text" name="name" id="name" required placeholder="name">
-    </div>
-    <div id="price-item">
-      <input type="text" name="price" id="price" required placeholder="price">
-    </div>
-    <div id="description-item">
-      <input type="text" name="description" id="description" required placeholder="description">
-    </div>
-    <div id="pircture-item">
-      <input type="text" name="pircture" id="pircture" >
-    </div>
-    <input type="submit" class="" value="Ajouter">
-  </form>
-</div>
-
-<?php
-  for ($i=0; $i < $_SESSION['nbcard']; $i++) { 
-    ?>
-<div class='card-wrapper'>
-    <div class='card' data-toggle-class='flipped'>
-      <div class='card-front'>
-        <div class='layer'>
-          <h1>cards</h1>
-          <div class='corner'></div>
-          <div class='corner'></div>
-          <div class='corner'></div>
-          <div class='corner'></div>
-        </div>
-      </div>
-      <div class='card-back'>
-        <div class='layer'>
-          <div class='top'>
-            <h2>Chief Idea Officer</h2>
-          </div>
-          <div class='bottom'>
-          </div>
-        </div>
-      </div>
-    </div>
+    <button id="add"> <img src="https://media.discordapp.net/attachments/950364547372425256/1034095033818828800/images.jpeg"> </button>
+    <a id="ok" class="prix">prix</a>
   </div>
 
-</div>
-<?php
-  }
-  ?>
+  <div id="form-item" class="hidden">
+    <form method=”POST” id="form">
+      <div id="name-item">
+        <input type="text" name="name" id="name" required placeholder="name">
+      </div>
+      <div id="price-item">
+        <input type="text" name="price" id="price" required placeholder="price">
+      </div>
+      <div id="description-item">
+        <input type="text" name="description" id="description" required placeholder="description">
+      </div>
+      <div id="pircture-item">
+        <input type="file" name="picture" id="picture">
+      </div>
+      <input type="submit" class="" value="Ajouter">
+    </form>
+  </div>
+
+  <div class="card">
+    <?php
+    for ($i = 0; $i < $_SESSION['nbcard']; $i++) {
+    ?>
+
+      <div class="flip-card">
+        <div class="flip-card-inner">
+          <div class="flip-card-front">
+            <img src="<?php echo $data[$i]['picture']; ?>" style="width:300px;height:300px;">
+          </div>
+          <div class="flip-card-back">
+            <h1>
+              <?php 
+                echo $data[$i]['name'];
+              ?>
+            </h1>
+            <p>Architect & Engineer</p>
+            <p>We love that guy</p>
+          </div>
+        </div>
+      </div>
+
+    <?php
+    }
+    ?>
+  </div>
 
 
 
-<script src="../JS/hpage.js"></script>  
+  <script src="../JS/hpage.js"></script>
 
 </body>
+
 </html>
